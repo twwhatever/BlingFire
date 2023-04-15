6e8269133d29dce04e09d17758221e0c1174b61a
Ted Wild <twild@fb.com>
2023-04-15 12:55:38 -0700

    Export new function

--- dist-pypi/blingfire/__init__.py
+++ dist-pypi/blingfire/__init__.py
@@ -102,6 +102,29 @@ def text_to_words(s):
     return o_bytes.value.decode('utf-8')
 
 
+def text_to_words_with_tags_with_model(h, s):
+
+    # get the UTF-8 bytes
+    s_bytes = s.encode("utf-8")
+
+    # allocate the output buffer
+    o_bytes = create_string_buffer(len(s_bytes) * 3)
+    o_bytes_count = len(o_bytes)
+
+    o_tags = (c_int32 * o_bytes_count)()
+
+    # identify words
+    o_len = blingfire.TextToWordsWithTagsWithModel(c_char_p(s_bytes), c_int(len(s_bytes)), byref(o_bytes), byref(o_tags), c_int(o_bytes_count), c_void_p(h))
+
+    num_tokens = o_bytes.value.count(ord(' ')) + 1
+
+    # check if no error has happened
+    if -1 == o_len or o_len > o_bytes_count:
+        return ''
+
+    # compute the unicode string from the UTF-8 bytes
+    return (o_bytes.value.decode('utf-8').split(' '), o_tags[:num_tokens])
+
 def text_to_words_with_model(h, s):
 
     # get the UTF-8 bytes
@@ -167,22 +190,22 @@ def text_to_hashes(s, word_n_grams, bucketSize):
     return np.frombuffer(o_bytes, dtype=c_int32, count=o_len)
 
 
-def text_to_token_with_offsets(s, text_to_token_f, split_byte):    
+def text_to_token_with_offsets(s, text_to_token_f, split_byte):
     # get the UTF-8 bytes
     s_bytes = s.encode("utf-8")
- 
+
     # allocate the output buffer
     o_bytes = create_string_buffer(len(s_bytes) * 2)
     o_bytes_count = len(o_bytes)
-    
+
     # buffers for word beging and end
     o_start_offsets = (c_int32 * o_bytes_count)()
     o_end_offsets = (c_int32 * o_bytes_count)()
- 
+
     # identify paragraphs
     o_len = text_to_token_f(
-        c_char_p(s_bytes), c_int(len(s_bytes)), 
-        byref(o_bytes), byref(o_start_offsets), byref(o_end_offsets), 
+        c_char_p(s_bytes), c_int(len(s_bytes)),
+        byref(o_bytes), byref(o_start_offsets), byref(o_end_offsets),
         c_int(o_bytes_count))
 
     # check if no error has happened
@@ -190,14 +213,14 @@ def text_to_token_with_offsets(s, text_to_token_f, split_byte):
         return '', []
 
     num_tokens = o_bytes.value.count(split_byte) + 1
-          
+
     utf8_offsets = [ o for start_end in zip(o_start_offsets[:num_tokens], o_end_offsets[:num_tokens]) for o in start_end]
-    
-    string_offsets = []    
-    
-    # Map the utf8 offsets to offsets in the original string - will break for "extended grapheme" 
-    # This seems to be undoing the FAUtf8Size based mapping being done inside TextToWordsWithOffsets and 
-    # TextToSentencesWithOffsets.     
+
+    string_offsets = []
+
+    # Map the utf8 offsets to offsets in the original string - will break for "extended grapheme"
+    # This seems to be undoing the FAUtf8Size based mapping being done inside TextToWordsWithOffsets and
+    # TextToSentencesWithOffsets.
     string_offset = 0
     is_end_offset = False
     for utf8_offset, b in enumerate(s_bytes):
@@ -206,22 +229,22 @@ def text_to_token_with_offsets(s, text_to_token_f, split_byte):
                 string_offsets.append(string_offset)
                 is_end_offset = not is_end_offset
             string_offset += 1
- 
+
     if len(string_offsets) < num_tokens * 2:
         string_offsets.append(len(s))
-            
+
     assert len(string_offsets) == num_tokens * 2, '%s != %s' % (len(string_offsets), num_tokens * 2)
- 
+
     token_begin_end = [ (b, e) for b, e in zip(string_offsets[::2], string_offsets[1::2])]
-        
+
     # compute the unicode string from the UTF-8 bytes
-    out_string = o_bytes.value.decode('utf8') 
-    
+    out_string = o_bytes.value.decode('utf8')
+
     return out_string, token_begin_end
- 
+
 def text_to_words_with_offsets(s):
     return text_to_token_with_offsets(s, blingfire.TextToWordsWithOffsets, ord(' '))
- 
+
 def text_to_sentences_and_offsets(s):
     return text_to_token_with_offsets(s, blingfire.TextToSentencesWithOffsets, ord('\n'))
 
@@ -279,8 +302,8 @@ def utf8text_to_ids_with_offsets(h, s_bytes, max_len, unk = 0, no_padding = Fals
     t_count = blingfire.TextToIdsWithOffsets(c_void_p(h), c_char_p(s_bytes), c_int(len(s_bytes)), byref(o_bytes), byref(o_bytes_starts), byref(o_bytes_ends), c_int(o_bytes_count), c_int(unk))
     out_count = min (o_bytes_count, t_count) if no_padding else o_bytes_count
     # return numpy array without copying
-    return ( np.frombuffer(o_bytes, dtype=c_uint32, count = out_count), 
-             np.frombuffer(o_bytes_starts, dtype=c_int32, count = out_count), 
+    return ( np.frombuffer(o_bytes, dtype=c_uint32, count = out_count),
+             np.frombuffer(o_bytes_starts, dtype=c_int32, count = out_count),
              np.frombuffer(o_bytes_ends, dtype=c_int32, count = out_count) )
 
 
